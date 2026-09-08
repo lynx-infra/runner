5d920617130f045648c483be73538b3cf3f70a54
fenghao.0721 <fenghao.0721@bytedance.com>
2026-09-08 15:24:50 +0800

    fix: decouple runner image packages from KubeVirt

--- images/kubevirt/validate-build-contract.sh
+++ images/kubevirt/validate-build-contract.sh
@@ -31,7 +31,6 @@ assert_docker_arg CMD_LINE_VERSION "$ANDROID_COMMAND_LINE_TOOLS_VERSION"
 assert_docker_arg RUNNER_USER_UID "$RUNNER_USER_UID"
 
 test "$(sort "$images_dir/manifests/ubuntu-24.04-system-packages.txt" | uniq -d | wc -l)" -eq 0
-grep -Fq 'manifests/ubuntu-24.04-system-packages.txt' "$dockerfile"
 grep -Fq "ENV ImageOS=$IMAGE_OS" "$dockerfile"
 grep -Fq "runner-container-hooks/releases/download/v${RUNNER_HOOKS_VERSION}/" "$dockerfile"
 grep -Fq "PYTHON_TOOLCACHE_VERSION: '${PYTHON_TOOLCACHE_VERSION}'" \
